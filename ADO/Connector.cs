acac87237747e88846b98243df9c52bfaf9e25d9
kit <kit@DESKTOP-EHKDJE1>
2026-02-21 12:30:21 +0300

    Add project files.

---
+++ ADO/Connector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace ADO
+{
+    internal class Connector
+    {
+        string connection_string;
+        SqlConnection connection;
+        public Connector(string connection_string)
+        {
+            this.connection_string = connection_string;
+            this.connection = new SqlConnection(connection_string);
+        }
+
+        public void Select(string cmd)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand(cmd, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write(reader[i].ToString().PadRight(29));
+                }
+                Console.WriteLine();
+            }
+
+            reader.Close();
+            connection.Close();
+            Connector connector = new Connector(connection_string);
+        }
+    }
+}
---
+++ ADO/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System.Data.SqlClient;
+
+namespace ADO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_SPU_411;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection connection = new SqlConnection(connection_string);
+
+            string cmd = "SELECT title,year,first_name,last_name FROM Movies JOIN Directors ON (director = director_id)";
+            // connection.Open();
+            //// string cmd = "SELECT * FROM Movies";
+            // SqlCommand command = new SqlCommand(cmd, connection);
+            // SqlDataReader reader = command.ExecuteReader();
+            // while (reader.Read())
+            // {
+            //     //Console.WriteLine($"{reader[0]} \t  {reader[1]} \t  {reader[2]}");
+            //     for (int i = 0; i < reader.FieldCount; i++)
+            //     {
+            //         //Console.Write(reader[i] +   "\t");
+            //         Console.Write(reader[i].ToString().PadRight(29));
+            //     }
+            //     Console.WriteLine();
+            // }
+
+            // reader.Close();
+            // connection.Close();
+            Connector connector = new Connector(connection_string);
+            connector.Select(cmd);
+        }
+    }
+}
