c37d00d25de0389355947fcb8d31eb905839e568
kit <kit@DESKTOP-EHKDJE1>
2026-03-07 11:34:41 +0300

    GetPrimaruKey

--- ADO/Connector.cs
+++ ADO/Connector.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 using System.Data.SqlClient;
 
-namespace ADO
+namespace IntroductionToADO
 {
-    internal class Connector
+    class Connector
     {
         string connection_string;
         SqlConnection connection;
@@ -17,28 +17,30 @@ namespace ADO
             this.connection_string = connection_string;
             this.connection = new SqlConnection(connection_string);
         }
-        public void Select(string fileds, string tables, string condition = "")
+        public void Select(string cmd)
         {
-            string cmd = $"SELECT {fileds} FROM {tables}";
+            connection.Open();
+            SqlCommand command = new SqlCommand(cmd, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                //Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write(reader[i].ToString().PadRight(29));
+                }
+                Console.WriteLine();
+            }
+            reader.Close();
+            connection.Close();
+        }
+        public void Select(string fields, string tables, string condition = "")
+        {
+            string cmd = $"SELECT {fields} FROM {tables}";
             if (condition != "") cmd += $" WHERE {condition}";
             cmd += ";";
             Select(cmd);
-            //connection.Open();
-            //SqlCommand command = new SqlCommand(cmd, connection);
-            //SqlDataReader reader = command.ExecuteReader();
-            //while (reader.Read())
-            //{
-            //    for (int i = 0; i < reader.FieldCount; i++)
-            //    {
-            //        Console.Write(reader[i].ToString().PadRight(29));
-            //    }
-            //    Console.WriteLine();
-            //}
-            //reader.Close();
-            //connection.Close();
-
         }
-
         public void Insert(string cmd)
         {
             connection.Open();
@@ -50,87 +52,62 @@ namespace ADO
         {
             string cmd = $"INSERT INTO {table} VALUES ({values})";
             Insert(cmd);
-            //connection.Open();
-            //SqlCommand command = new SqlCommand(cmd, connection);
-            //command.ExecuteNonQuery();
-            //connection.Close();
-
-        }
-        public void Select(string cmd)
-        {
-
-            connection.Open();
-            SqlCommand command = new SqlCommand(cmd, connection);
-            SqlDataReader reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                for (int i = 0; i < reader.FieldCount; i++)
-                {
-                    Console.Write(reader[i].ToString().PadRight(29));
-                }
-                Console.WriteLine();
-            }
-            reader.Close();
-            connection.Close();
-
         }
         public object Scalar(string cmd)
         {
             SqlCommand command = new SqlCommand(cmd, connection);
             connection.Open();
-            //int value = Convert.ToInt32(command.ExecuteScalar());
             object value = command.ExecuteScalar();
+            //int value = Convert.ToInt32(command.ExecuteScalar());
             connection.Close();
             return value;
         }
         public string GetPrimaryKeyColumn(string table)
         {
             return (string)Scalar
-                (
-                $"SELECT COLUMN_NAME" +
-                $" FROM INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE " +
-                $"WHERE CONSTRAINT_NAME = (SELECT CONSTRAINT_NAME" +
-                $" FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS " +
-                $"WHERE TABLE_NAME = N'{table}' AND CONSTRAINT_TYPE = N'PRIMARY KEY')"
-                );
+        (
+             $"SELECT COLUMN_NAME " +
+             $"FROM INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE " +
+             $"WHERE CONSTRAINT_NAME = (SELECT CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE TABLE_NAME = N'{table}' AND CONSTRAINT_TYPE=N'PRIMARY KEY')"
+        );
+        }
+        public object GetPrimaryKey(string cmd)
+        {
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+            object primary_key = command.ExecuteScalar();
+            connection.Close();
+            return primary_key;
+        }
+        public object GetPrimaryKey(string table, string fields, string values)
+        {
+            string[] s_fields = fields.Split(',');
+            string[] s_values = values.Split(',');
+            if (s_fields.Length != s_values.Length) return null;
+            string condition = "";
+            for (int i = 0; i < s_values.Length; i++)
+            {
+                condition += $"{s_fields[i].Trim()}=N'{s_values[i].Trim()}'";
+                if (i != s_values.Length - 1) condition += " AND ";
+            }
+            string cmd = $"SELECT {GetPrimaryKeyColumn(table)} FROM {table} WHERE {condition}";
+            return Scalar(cmd);
         }
         public int GetLastPrimaryKey(string table)
         {
-            return Convert.ToInt32(Scalar($"SELECT MAX({GetPrimaryKeyColumn(table)})FROM {table}")) ;
-            /*(
-            $"SELECT CONSTRAINT_NAME" +
-            $" FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS" +
-            $" WHERE CONSTRAINT_TYPE = N'PRIMARY KEY " +
-            $"'AND TABLE_NAME = N'{table}';"
-            );
-
-            string primary_key_column = result as string;
-            Console.WriteLine(primary_key_column);
-            return primary_key_column;*/
-
+            return Convert.ToInt32(Scalar($"SELECT MAX({GetPrimaryKeyColumn(table)}) FROM {table}"));
         }
         public int GetNextPrimaryKey(string table)
         {
             return GetLastPrimaryKey(table) + 1;
         }
-         public void Update(string cmd)
-        {
-            SqlCommand command = new SqlCommand (cmd, connection);
-            connection.Open();
-            command.ExecuteNonQuery();
-            connection.Close ();
 
-        }
-        public void Delete(string table, string condition)
+        public void Update(string cmd)
         {
-            string cmd = $"DELETE FROM {table} WHERE {condition};";
-
-            connection.Open();
             SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
             command.ExecuteNonQuery();
             connection.Close();
         }
-
-
     }
 }
--- ADO/Program.cs
+++ ADO/Program.cs
@@ -4,59 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 using System.Data.SqlClient;
 
-namespace ADO
+namespace IntroductionToADO
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_SPU_411;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             SqlConnection connection = new SqlConnection(connection_string);
+            string cmd = "SELECT title,year,first_name,last_name FROM Movies JOIN Directors ON(director=director_id)";
 
-            string cmd = "SELECT title,year,first_name,last_name FROM Movies JOIN Directors ON (director = director_id)";
-            // connection.Open();
-            //// string cmd = "SELECT * FROM Movies";
-            // SqlCommand command = new SqlCommand(cmd, connection);
-            // SqlDataReader reader = command.ExecuteReader();
-            // while (reader.Read())
-            // {
-            //     //Console.WriteLine($"{reader[0]} \t  {reader[1]} \t  {reader[2]}");
-            //     for (int i = 0; i < reader.FieldCount; i++)
-            //     {
-            //         //Console.Write(reader[i] +   "\t");
-            //         Console.Write(reader[i].ToString().PadRight(29));
-            //     }
-            //     Console.WriteLine();
-            // }
-
-            // reader.Close();
-            // connection.Close();
             Connector connector = new Connector(connection_string);
-            connector.Select(" title,year,first_name,last_name", "Movies,Directors", "director = director_id");
+            connector.Select("title,year,first_name,last_name", "Movies,Directors", "director=director_id");
             Console.WriteLine("\n-------------------------------------------------------------\n");
 
             string table = "Movies";
-            // Console.WriteLine(connector.Scalar("Select MAX(director_id) FROM Directors"));
+            //Console.WriteLine(connector.Scalar($"SELECT MAX(director_id) FROM Directors"));
             Console.WriteLine(connector.GetLastPrimaryKey(table));
             Console.WriteLine(connector.GetNextPrimaryKey(table));
             Console.WriteLine(connector.GetPrimaryKeyColumn(table));
-            // connector.GetNextPrimaryKey(table);
-            /*  connector.Insert
+            Console.WriteLine("\n-------------------------------------------------------------\n");
+            connector.Insert
                 (
-                $"IF NOT EXISTS(SELECT * FROM Directors WHERE last_name = N'Besson' AND first_name = N'Luc')" +
-                $" INSERT Directors VALUES ({connector.GetNextPrimaryKey("Directors")}, N'Scott', N'Gray')"
+                     $"IF NOT EXISTS (SELECT * FROM Directors WHERE last_name=N'Scott' AND first_name=N'Gray') " +
+                     $"INSERT Directors VALUES({connector.GetNextPrimaryKey("Directors")},N'Scott',N'Gray')"
                 );
-            */
-
-            //connector.Insert("Directors", $"{connector.GetNextPrimaryKey("Directors")}, N'Besson',N'Luc'");
-            //connector.Update("UPDATE Directors SET last_name = N'Lettich', first_name = N'Sheldon' WHERE director_id = 10");
-           // connector.Delete("Directors", "director_id = 13");
+            //connector.Insert("Directors", $"{connector.GetNextPrimaryKey("Directors")},N'Besson',N'Luc'");
+            connector.Update("UPDATE Directors SET last_name=N'Lettich',first_name=N'Sheldon' WHERE director_id=8");
             connector.Select("*", "Directors");
             Console.WriteLine("\n-------------------------------------------------------------\n");
+
             connector.Select(cmd);
+
+            Console.WriteLine(connector.GetPrimaryKey("SELECT director_id FROM Directors WHERE last_name=N'Cameron' AND first_name=N'James'"));
+            Console.WriteLine(connector.GetPrimaryKey("Directors", "  last_name,  first_name ", "   Cameron  ,  James   "));
+            Console.WriteLine(connector.GetPrimaryKey("Movies", "  title,  director ", "The Heat, 5"));
+            Console.WriteLine(connector.GetPrimaryKey("Movies", "  title,  year ", "The Heat, 1995-12-15"));
         }
     }
 }
